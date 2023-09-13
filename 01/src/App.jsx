fe7fbc0b694bac1879f1a440c80f8c2ae8fcb639
Shashank Shukla <64839400+Shashank-GitHub-23@users.noreply.github.com>
2023-09-13 14:19:31 +0530

    NavBar

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -5,11 +5,31 @@ import Home from './assets/Component/Home';
 import About from './assets/Component/About';
 
 
+
 function App() {
   
 
   return (
+
+   
     <>
+    <div  >
+      <div className='navbar'>
+        <div className="logo">
+       <img src="Images/brand_logo.png" alt="Logo" srcset="" />
+       </div>
+       
+       <ul className='nav-buttons'> 
+        <li className='nav-button' >Menu</li>
+        <li className='nav-button' >Location</li>
+        <li className='nav-button' >About</li>
+        <li className='nav-button' >Contact</li>
+       </ul>
+
+       <button className='login-button'>Login</button>
+       
+       </div>
+       </div>
       <BrowserRouter>
       <Routes>
         < Route path="/" element={<Home />} />
---
+++ 01/src/assets/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="01\Images\brand_logo.png" alt="" srcset="" />
+    </div>
+  )
+}
+
+export default Navbar
