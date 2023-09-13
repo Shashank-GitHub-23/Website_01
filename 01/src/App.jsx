9b69707be3276f9e2c42ce3da362dd909d69d4ed
Shashank Shukla <64839400+Shashank-GitHub-23@users.noreply.github.com>
2023-09-13 16:16:25 +0530

    Nav bar

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './assets/Component/Home';
 import About from './assets/Component/About';
+import Navbar from './assets/Component/Navbar';
+import './navbar.css';
 
 
 
@@ -13,23 +15,7 @@ function App() {
 
    
     <>
-    <div  >
-      <div className='navbar'>
-        <div className="logo">
-       <img src="Images/brand_logo.png" alt="Logo" srcset="" />
-       </div>
-       
-       <ul className='nav-buttons'> 
-        <li className='nav-button' >Menu</li>
-        <li className='nav-button' >Location</li>
-        <li className='nav-button' >About</li>
-        <li className='nav-button' >Contact</li>
-       </ul>
-
-       <button className='login-button'>Login</button>
-       
-       </div>
-       </div>
+     <Navbar />
       <BrowserRouter>
       <Routes>
         < Route path="/" element={<Home />} />
--- 01/src/assets/Component/Navbar.jsx
+++ 01/src/assets/Component/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
+
 const Navbar = () => {
+
   return (
-    <div>
-      <img src="01\Images\brand_logo.png" alt="" srcset="" />
-    </div>
+    <div  >
+        <div className='navbar'>
+        <div className="logo">
+        <img src="Images/brand_logo.png" alt="Logo" srcset="" />
+        </div>
+       
+        <ul className='nav-buttons'> 
+        <li className='nav-button' >Menu</li>
+        <li className='nav-button' >Location</li>
+        <li className='nav-button' >About</li>
+        <li className='nav-button' >Contact</li>
+       </ul>
+
+       <button className='login-button'>Login</button>
+       
+       </div>
+       </div>
   )
 }
 
